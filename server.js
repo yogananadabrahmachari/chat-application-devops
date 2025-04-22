646745bcbf051cd2b0c2554edc41c821fadf7d46
Chari <158556540+yogananadabrahmachari@users.noreply.github.com>
2025-04-22 19:27:35 +0530

    server.js

--- server.js
+++ server.js
@@ -85,4 +85,5 @@ app.get('/chat', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
